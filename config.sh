7c1a47f37115d791028d1252b32c53d83826ec47
Your Name <you@example.com>
2022-12-18 08:29:20 +0700

    new file:   config.sh 	modified:   scan.sh

---
+++ config.sh
@@ -0,0 +1,7 @@
+export DEFAULT_SNI_HOST="sanggoro5.nextvpn.cc" ## sni_curl,sni_openssl
+export DEFAULT_USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.100" ## http_status,https_status,proxy
+export BLACKLIST_REDIRECT="http://filter.ncnd.indosatooredoo.com/nc/" ## http_status,https_status,proxy
+export DEFAULT_CONNECT_TIMEOUT="5" ## sni_curl
+export DEFAULT_MAX_TIMEOUT="5" ## sni_curl
+export DEFAULT_TIMEOUT="10" ## sni_openssl
+export DEFAULT_THREAD="100" ## bulk
